f31ad79dac381f40c4e45914b57d2b87e2077087
Giovana <grbeltrame@gmail.com>
2023-11-07 15:36:43 -0300

    classe imagem finalizada

---
+++ imagem.py
@@ -0,0 +1,38 @@
+class Imagem:
+
+
+# Construtor da classe imagem, recebe os atributos da imagem de cada processo
+  def __init__(self,id_processo, PC, IR, estado):
+    self.id_processo = id_processo
+    self.PC = PC
+    self.IR = IR
+    self.estado = estado
+    self.executavel = []
+
+# Adiciona linhas de codigo do tipo Registro Entrada no arquivo de codigo executavel
+# Usar essa função na hora de ler o arquivo de entrada, o parametro registro é uma linha do arquivo de entrada
+  def adiciona_registro(self, registro):
+    self.executavel.append(registro)
+
+# Função para atualizar a imagem
+# Quando o processo muda de estado, a imagem pode ser atualizada. Usar quando necessario, isto é, quando vai da MP para MS e vice versa
+# ou seja, quando vai e sai dos estados suspensos
+  def atualiza_imagem(self, new_PC, new_IR, new_Estado):
+    self.PC = new_PC
+    self.IR = new_IR
+    self.estado = new_Estado
+
+# Função para atualizar apenas o estado do processo, quando ele esta indo de PRONTO para EXECUTANDO, de EXECUTANDO para BLOQUEADO, 
+# de BLOQUEADO para PRONTO e quando ele é FINALIZADO
+  def atualiza_estado(self, new_Estado):
+    self.estado = new_Estado
+
+#  Função para mostrar o estado da imagem
+  def mostra_imagem(self):
+    print('\nSITUAÇÃO DA IMAGEM:')
+    print('----------------------')
+    print(f'Id do processo: {self.id_processo}')
+    print(f'Program counter: {self.PC}')
+    print(f'Instruction registrer: {self.IR}')
+    print(f'Estado do processo: {self.estado}\n')
+  
